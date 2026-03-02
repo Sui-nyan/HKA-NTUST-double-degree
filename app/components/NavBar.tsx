30dba8fbc5ee61c3ace008e67f0867fd470e05ac
sui-nyan <78491387+Sui-nyan@users.noreply.github.com>
2026-03-02 04:56:07 +0100

    Create NavBar.tsx

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,11 @@
+"use client";
+import {AppBar, Container} from "@mui/material";
+
+export default function NavBar() {
+    return (
+        <AppBar position={"fixed"}>
+            <Container maxWidth={"xl"}>
+            </Container>
+        </AppBar>
+    )
+}
