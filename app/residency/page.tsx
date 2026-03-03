0db5db9fdb9a38a2f7de14b790dddcc69cb79c83
sui-nyan <78491387+Sui-nyan@users.noreply.github.com>
2026-03-03 02:49:36 +0100

    extend timeline content, add pages

--- app/residency/page.tsx
+++ app/residency/page.tsx
@@ -7,13 +7,23 @@ import {useState} from "react";
 import Link from "next/link";
 
 export default function ClassSchedulePage() {
-  return (
-    <Box className="flex flex-col items-center gap-8">
-        <Typography variant="h1">Applying for residency</Typography>
-        <Box>
-            <Typography variant={"body2"}>Nach dem ihr die ersten vier Steps eurer Registrierung durchgeführt habt, bekommt ihr ein Certificate of Enrollment. Mit diesem Dokument könnt ihr nun die Applikation für Residency ausfüllen.</Typography>
-            <Button variant="contained"><Link href={"https://coa.immigration.gov.tw/coa-frontend/foreign-student/individual/login?verify=true"}>Zur Anlaufstelle</Link></Button>
+    return (
+        <Box className="flex flex-col items-center gap-8">
+            <Typography variant="h1">Applying for residency</Typography>
+            <Box>
+                <Typography variant={"body2"}>
+                    Nach dem ihr die ersten vier Steps eurer Registrierung durchgeführt habt, bekommt ihr ein
+                    Certificate of Enrollment. Mit diesem Dokument könnt ihr nun die Applikation für Residency
+                    ausfüllen.</Typography>
+                <Button variant="contained"><Link
+                    href={"https://coa.immigration.gov.tw/coa-frontend/foreign-student/individual/login?verify=true"}>Zur
+                    Anlaufstelle</Link></Button>
+            </Box>
+            <Box>
+                <Typography variant="body2" component="p">
+
+                </Typography>
+            </Box>
         </Box>
-    </Box>
-  );
+    );
 }
--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -11,128 +11,147 @@ export default function TimelinePage() {
                 <Timeline position="alternate">
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>Ein Semester davor</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>Ein Semester davor</Typography>
                                 <Typography variant="h5">Anmeldung Double Degree Program</Typography>
                                 <Typography variant="body2">
-                                    Am besten bei der Infoveranstaltung für den Double Degree teilnehmen oder direkt eine Mail an die Projektverantwortliche Andrea Wirth senden.
+                                    Am besten bei der Infoveranstaltung für den Double Degree teilnehmen oder direkt
+                                    eine Mail an die Projektverantwortliche Andrea Wirth senden.
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>1-2 Monate vorher</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>1-2 Monate vorher</Typography>
                                 <Typography variant="h5">Visa Vorbereitung</Typography>
                                 <Typography variant="body2">
-                                    Für das Visum muss ein Health Check vorbereitet werden. Anreisende aus Deutschland brauchen eine Untersuchung auf Tuberkulosis, Syphilis-Test, Impfnachweise für Masern, ...
+                                    Für das Visum muss ein Health Check vorbereitet werden. Anreisende aus Deutschland
+                                    brauchen eine Untersuchung auf Tuberkulosis, Syphilis-Test, Impfnachweise für
+                                    Masern, ...
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>1 Monat vor Abreise</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>1 Monat vor Abreise</Typography>
                                 <Typography variant="h5">Visa-Antrag absenden</Typography>
                                 <Typography variant="body2">
-                                    Das Konsulat arbeitet sehr schnell und hat das Visum meistens innerhalb von 5 Tagen vorbereitet.
-                                    Lass trotzdem am besten ein bisschen Puffer, damit notfalls noch Dokumente nachgesendet werden können.
+                                    Das Konsulat arbeitet sehr schnell und hat das Visum meistens innerhalb von 5 Tagen
+                                    vorbereitet.
+                                    Lass trotzdem am besten ein bisschen Puffer, damit notfalls noch Dokumente
+                                    nachgesendet werden können.
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>Anreise</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>Anreise</Typography>
                                 <Typography variant="h5">Flug</Typography>
                                 <Typography variant="body2">
-                                    Ein direkt Flug von Deutschland nach Taiwan ist beispielsweise über Frankfurt oder München möglich. Über Stuttgart gibt es auch die Möglichkeit mit einem Umstieg zu fliegen.
-                                    Da das Vorausbuchen von Flügen nur in den nächsten 10 Monaten möglich ist, müsst ihr vorerst nur den Hinflug buchen.
+                                    Ein direkt Flug von Deutschland nach Taiwan ist beispielsweise über Frankfurt oder
+                                    München möglich. Über Stuttgart gibt es auch die Möglichkeit mit einem Umstieg zu
+                                    fliegen.
+                                    Da das Vorausbuchen von Flügen nur für die nächsten 10 Monaten möglich ist, könnt
+                                    ihr vorerst nur den Hinflug buchen.
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>Ankunft</Typography>
-                                <Typography variant="h5"><Link href={"/campus-registration"}>Erster Campus Tag</Link></Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>Ankunft</Typography>
+                                <Typography variant="h5"><Link href={"/campus-registration"}>Erster Campus
+                                    Tag</Link></Typography>
                                 <Typography variant="body2">
-                                    Für die Registration am Campus müssen Studenten zum International Office. Es gibt eine Koordinatorin, die spezifisch für Double Degree Studenten zuständig ist.
-                                    Sobald ihr einmal da wart bekommt ihr ein Laufzettel, der in richtiger Reihenfolge ausgefüllt werden muss.
-                                    Nach dem ihr die Steps bis 4 durchgemacht habt kann auch der Antrag für die Residency gemacht werden.
+                                    Für die Registration am Campus müssen Studenten zum International Office. Es gibt
+                                    eine Koordinatorin, die spezifisch für Double Degree Studenten zuständig ist.
+                                    Sobald ihr einmal da wart bekommt ihr ein Laufzettel, der in richtiger Reihenfolge
+                                    ausgefüllt werden muss.
+                                    Nach dem ihr die Steps bis 4 durchgemacht habt kann auch der Antrag für die
+                                    Residency gemacht werden.
+                                    Außerdem sollte der <Link href={""}>Credit Transfer</Link> so bald wie möglich gemacht werden.
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>Ankunft</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>Ankunft</Typography>
                                 <Typography variant="h5"><Link href={"/residency"}>ARC Antrag</Link></Typography>
                                 <Typography variant="body2">
-                                    Um die Alien Resident Card (ARC) zu bekommen muss zuerst ein Antrag über die Webseite der Immigration Agency beantragt werden.
-                                    Eine Kopie der ARC ist außerdem notwendig damit ihr eure Admission vervollständigen könnt und eure Studentenkarte bekommt.
+                                    Um die Alien Resident Card (ARC) zu bekommen muss zuerst ein Antrag über die
+                                    Webseite der Immigration Agency beantragt werden.
+                                    Eine Kopie der ARC ist außerdem notwendig damit ihr eure Admission vervollständigen
+                                    könnt und eure Studentenkarte bekommt.
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>Angekommen</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>Angekommen</Typography>
                                 <Typography variant="h5"></Typography>
                                 <Typography variant="body2">
-                                    Sobald ihr eure Studentenkarte und eure ARC bekommen habt könnt ihr für billigere Preise eine taiwanesische Sim-Karte erhalten.
-                                    Außerdem habt ihr jetzt auch die Möglichkeit ein Bankkonto zu eröffnen.
+                                    Sobald ihr eure Studentenkarte und eure ARC bekommen habt könnt ihr für billigere
+                                    Preise eine taiwanesische <Link href={"/mobile"}>Sim-Karte</Link> erhalten.
+                                    Außerdem habt ihr jetzt auch die Möglichkeit ein <Link href={"/banking"}>Bankkonto</Link> zu eröffnen.
                                 </Typography>
                             </Card>
                         </TimelineContent>
                     </TimelineItem>
                     <TimelineItem>
                         <TimelineSeparator>
-                            <TimelineDot />
-                            <TimelineConnector />
+                            <TimelineDot/>
+                            <TimelineConnector/>
                         </TimelineSeparator>
                         <TimelineContent>
                             <Card>
-                                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>University Life</Typography>
+                                <Typography sx={{color: 'text.secondary', mb: 1.5}}>University Life</Typography>
                                 <Typography variant="h5"></Typography>
                                 <Typography variant="body2">
-                                    In den ersten zwei Wochen der Vorlesung gibt es die Möglichkeit Kurse hinzuzufügen oder zu droppen.
-                                    Meistens sind die Kurse sehr schnell ausgebucht, es macht also Sinn sich früh einzuschreiben und eventuell später nochmal zu droppen.
-                                    Sobald die Add-Drop Phase vorbei ist ist es sehr schwierig noch in Kurse reinzukommen oder diese zu droppen.
+                                    In den ersten zwei Wochen der Vorlesung gibt es die Möglichkeit Kurse hinzuzufügen
+                                    oder zu droppen.
+                                    Meistens sind die Kurse sehr schnell ausgebucht, es macht also Sinn sich früh
+                                    einzuschreiben und eventuell später nochmal zu droppen.
+                                    Sobald die Add-Drop Phase vorbei ist ist es sehr schwierig noch in Kurse
+                                    reinzukommen oder diese zu droppen.
                                 </Typography>
                             </Card>
                         </TimelineContent>
