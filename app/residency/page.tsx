97db2f0ae929448345391e0f6fd6851c852b2d12
Elisa <78491387+Sui-nyan@users.noreply.github.com>
2026-04-22 16:41:36 +0200

    Content (#6)
    * refactoring
    
    * banking info
    
    * Update Residency info
    
    * en is also de
    
    * Update de.json

--- app/residency/page.tsx
+++ app/residency/page.tsx
@@ -1,29 +1,30 @@
-"use client";
-
 import {Box} from "@mui/system";
-import {Button, InputLabel, MenuItem, Select, SelectChangeEvent, Typography} from "@mui/material";
-import Form from "next/form";
-import {useState} from "react";
-import Link from "next/link";
+import {Button, Container, Link, Typography} from "@mui/material";
+import {InfoCard} from "@/app/components/InfoCard";
+import localeData from "./locales/en.json";
+import type { CourseSelectionLocale } from "../../lib/types";
 
-export default function ClassSchedulePage() {
+const locale = localeData as unknown as CourseSelectionLocale;
+
+export default function ResidencyPage() {
+    const cards = locale.infoCards ?? [];
+
     return (
-        <Box className="flex flex-col items-center gap-8">
-            <Typography variant="h1">Applying for residency</Typography>
-            <Box>
-                <Typography variant={"body2"}>
-                    Nach dem ihr die ersten vier Steps eurer Registrierung durchgeführt habt, bekommt ihr ein
-                    Certificate of Enrollment. Mit diesem Dokument könnt ihr nun die Applikation für Residency
-                    ausfüllen.</Typography>
-                <Button variant="contained"><Link
-                    href={"https://coa.immigration.gov.tw/coa-frontend/foreign-student/individual/login?verify=true"}>Zur
-                    Anlaufstelle</Link></Button>
-            </Box>
-            <Box>
-                <Typography variant="body2" component="p">
-
-                </Typography>
-            </Box>
+        <Box>
+            <Typography variant="h1">{locale.pageTitle ?? 'ARC-Antrag'}</Typography>
+            <Typography variant={"body2"}>
+                        Nach dem ihr die ersten vier Steps eurer Registrierung durchgeführt habt, bekommt ihr ein
+                        Certificate of Enrollment. Mit diesem Dokument könnt ihr nun die Applikation für Residency
+                        ausfüllen.</Typography>
+            <Container className="gap-4 flex flex-col">
+                {cards.map((c, i) => (
+                    <InfoCard key={i} title={c.title} description={c.description} />
+                ))}
+                <Box>
+                    <Button variant="contained"><Link
+                        href={"https://coa.immigration.gov.tw/coa-frontend/foreign-student/individual/login?verify=true"}>Zum ARC Antrag für Studenten</Link></Button>
+                </Box>
+            </Container>
         </Box>
     );
 }
